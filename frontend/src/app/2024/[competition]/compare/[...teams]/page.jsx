4582998235438746e0ce716d79655098a7bc7511
dmanslick <dvvshah@gmail.com>
2024-04-19 21:06:28 -0500

    removed compare cache

--- frontend/src/app/2024/[competition]/compare/[...teams]/page.jsx
+++ frontend/src/app/2024/[competition]/compare/[...teams]/page.jsx
@@ -4,7 +4,7 @@ import TruePage from './truePage'
 const getTeamData = async (params) => {
     const teams = Array.from(new Set(params.teams))
     const teamsData = await Promise.all(teams.map(async (team) => {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL_2024}/api/${params.competition}/${team}`)
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL_2024}/api/${params.competition}/${team}`, { cache: 'no-cache' })
         return response.json()
     }))
     return teamsData
